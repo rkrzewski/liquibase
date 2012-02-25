912bdaf6d8535e7448bbeb033c0fb8cd9060c027
Rafał Krzewski <rafal.krzewski@caltha.pl>
2012-02-25 02:33:56 +0100

    Moved DataSourceProvider's required settings to component configuration.

--- osgi/liquibase-osgi-test/src/main/java/liquibase/samples/osgi/DataSourceProvider.java
+++ osgi/liquibase-osgi-test/src/main/java/liquibase/samples/osgi/DataSourceProvider.java
@@ -1,10 +1,13 @@
 package liquibase.samples.osgi;
 
+import static aQute.bnd.annotation.component.ConfigurationPolicy.require;
+
 import java.io.OutputStreamWriter;
 import java.io.Writer;
 import java.sql.Connection;
 import java.util.Dictionary;
 import java.util.Hashtable;
+import java.util.Map;
 import java.util.Properties;
 
 import javax.sql.DataSource;
@@ -20,54 +23,55 @@ import aQute.bnd.annotation.component.Component;
 import aQute.bnd.annotation.component.Deactivate;
 import aQute.bnd.annotation.component.Reference;
 
-@Component
+@Component(configurationPolicy = require)
 public class DataSourceProvider {
-
-	private static final String JDBC_URL = "jdbc:derby:target/derby/db;create=true";
-
-	private static final String CHANGELOG = "META-INF/liquibase/changelog.xml";
 
 	private Liquibase liquibase;
 
 	private DataSourceFactory dsf;
-	
+
 	private ServiceRegistration<DataSource> dsReg;
 
 	@Reference
-	protected void setLiquibaseService(Liquibase liquibase) {
+	protected void setLiquibase(Liquibase liquibase) {
 		this.liquibase = liquibase;
 	}
 
-	@Reference(target = "(&(osgi.jdbc.driver.class=org.apache.derby.jdbc.EmbeddedDriver)"
-			+ "(osgi.jdbc.driver.version=4.0))")
+	@Reference
 	protected void setDataSourceFactory(DataSourceFactory dsf) {
 		this.dsf = dsf;
 	}
 
 	@Activate
-	protected void activate(BundleContext context) {
+	protected void activate(BundleContext context, Map<String, String> compProps) {
 		Properties dsfProps = new Properties();
-		dsfProps.put(DataSourceFactory.JDBC_URL, JDBC_URL);
+		for(String key : compProps.keySet()) {
+			if(key.startsWith("jdbc.")) {
+				dsfProps.put(key.substring(5), compProps.get(key));
+			}
+		}
+
 		try {
 			DataSource ds = dsf.createDataSource(dsfProps);
-			Connection conn = ds.getConnection();
 
-			liquibase.open(CHANGELOG, conn);
+			Connection conn = ds.getConnection();
+			String changeLogFile = compProps.get("liquibase.changelog");
+			liquibase.open(changeLogFile, conn);
 			Writer out = new OutputStreamWriter(System.out);
 			liquibase.update(null, out);
 			conn.close();
-			
-			Dictionary<String,Object> dsProps = new Hashtable<String,Object>();
+
+			Dictionary<String, Object> dsProps = new Hashtable<String, Object>();
 			dsProps.put("liquibase.updated", "true");
 			dsReg = context.registerService(DataSource.class, ds, dsProps);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	@Deactivate
 	protected void deactivate(BundleContext context) {
-		if(dsReg != null) {
+		if (dsReg != null) {
 			dsReg.unregister();
 		}
 	}
