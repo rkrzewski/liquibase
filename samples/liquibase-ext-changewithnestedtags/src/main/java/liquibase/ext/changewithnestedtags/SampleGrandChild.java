1d49f8362ab80d31d52f8c13ac991a4d8a2d3d6c
Rafał Krzewski <rafal.krzewski@caltha.pl>
2012-03-06 01:17:23 +0100

    Nested change tags need to implement SampleChange interface (or derivative thereof), otherwise unstable checksums are generated.

--- samples/liquibase-ext-changewithnestedtags/src/main/java/liquibase/ext/changewithnestedtags/SampleGrandChild.java
+++ samples/liquibase-ext-changewithnestedtags/src/main/java/liquibase/ext/changewithnestedtags/SampleGrandChild.java
@@ -1,6 +1,13 @@
 package liquibase.ext.changewithnestedtags;
 
-public class SampleGrandChild {
+import liquibase.change.custom.CustomTaskChange;
+import liquibase.database.Database;
+import liquibase.exception.CustomChangeException;
+import liquibase.exception.SetupException;
+import liquibase.exception.ValidationErrors;
+import liquibase.resource.ResourceAccessor;
+
+public class SampleGrandChild implements CustomTaskChange {
     private String columnName;
 
     public String getColumnName() {
@@ -14,4 +21,29 @@ public class SampleGrandChild {
     public SampleChild createGreatGrandChild() {
         return new SampleChild();
     }
+
+	public String getConfirmationMessage() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void setUp() throws SetupException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setFileOpener(ResourceAccessor resourceAccessor) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public ValidationErrors validate(Database database) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void execute(Database database) throws CustomChangeException {
+		// TODO Auto-generated method stub
+		
+	}
 }
